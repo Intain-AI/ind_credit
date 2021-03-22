54f18cd1f3b3dd85235f64d606454766de97c542
subashini107 <subashini@in-d.ai>
2021-03-22 18:54:12 +0530

    NER that extracts necessary fields from bank statements

--- Named_Entity_Recognition/NER_Extractor.py
+++ Named_Entity_Recognition/NER_Extractor.py
@@ -1,14 +1,16 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar 18 15:14:28 2021
 
-@author: DELL
-"""
 import json
-from Output_of_NER import output
 import Features_For_CRF as Features
 import pickle
-
+def output(word,tag):
+   IOB_tags=['B_ACC_NUMBER', 'I_ACC_NUMBER', 'B_ACC_HOLDER_NAME', 'I_ACC_HOLDER_NAME', 'B_IFSC_CODE','I_IFSC_CODE', 'B_EMAIL_ID','I_EMAIL_ID',  'B_ACC_OPEN_DATE', 'I_ACC_OPEN_DATE', 'B_JOINT_HOLDERS', 'I_JOINT_HOLDERS']
+   classes=["Account_Number","Account_Holder_Name","IFSC_Code","Email_ID","Account_Open_Date","Joint_Holders"]
+   result={}
+   for i in range(len(tag)):
+       ind=IOB_tags.index(tag[i])
+       key=classes[int(ind/2)]
+       result[key]=word[i]
+   return(result)
 def predict_input(sentences):
     X_test=[Features.sent_to_features(s,0) for s in sentences]
     with open("CRF_classifier.clf", 'rb') as clf:
@@ -26,9 +28,6 @@ def predict_input(sentences):
 def main():
    f = open('E-HDFC_17.json',) 
    data = json.load(f) 
-  
-   for value in data['result'].items():
-        text_list=data['result']['Page_1']['digitized_details']['logical_cells']
    for value in data['result'].items():
         text_list=data['result']['Page_1']['digitized_details']['logical_cells']
    sentences=[]
@@ -41,11 +40,12 @@ def main():
             tokens.append(sentences[i][j]['text'])
         sent.append(tokens)
 
-   f.close() 
+
   
    word,tag=predict_input(sent)
    result=output(word,tag)
    print(result)
+   
     
 if __name__=="__main__":
     main()
