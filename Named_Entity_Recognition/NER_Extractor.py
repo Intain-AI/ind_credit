72e3e7eb9182fea3d2f1308c0788eb8763eec9da
subashini107 <subashini@in-d.ai>
2021-04-01 13:11:19 +0530

    Coordinates and logical cell position of extracted fields are added

--- Named_Entity_Recognition/NER_Extractor.py
+++ Named_Entity_Recognition/NER_Extractor.py
@@ -2,50 +2,66 @@
 import json
 import Features_For_CRF as Features
 import pickle
-def output(word,tag):
+from Functionalities import preprocess,process_input_file,find_coordinates
+
+def output(word,tag,sentences,text_list):
    IOB_tags=['B_ACC_NUMBER', 'I_ACC_NUMBER', 'B_ACC_HOLDER_NAME', 'I_ACC_HOLDER_NAME', 'B_IFSC_CODE','I_IFSC_CODE', 'B_EMAIL_ID','I_EMAIL_ID',  'B_ACC_OPEN_DATE', 'I_ACC_OPEN_DATE', 'B_JOINT_HOLDERS', 'I_JOINT_HOLDERS']
-   classes=["Account_Number","Account_Holder_Name","IFSC_Code","Email_ID","Account_Open_Date","Joint_Holders"]
-   result={}
+   classes=["account_number","account_holder_name","ifsc_code","email_id","ac_open_date","joint_holders"]
+   label=["Account Number","Account Holder","IFSC Code","Email ID","Account Open Date","Joint Holders"]
+   
+   re=[]
+   extracted_fields=[]
    for i in range(len(tag)):
+       result={}
        ind=IOB_tags.index(tag[i])
        key=classes[int(ind/2)]
-       result[key]=word[i]
-   return(result)
+       l=label[int(ind/2)]
+       id,logical_id,coordinates=find_coordinates(sentences,text_list,word[i])
+       result["coordinates"]=coordinates
+       result["label"]=l
+       result["value"]=word[i]
+       result["varname"]=key    
+       result["logical_cell_position"]=logical_id
+       re.append(result)
+   
+   for i in range(len(re)):
+       extracted_fields.append(re[i]['label'])
+       
+   for i in label:
+       result={}
+       if i not in extracted_fields:
+           ind=label.index(i)
+           result['label']=i
+           result['value']='NA'
+           result["varname"]=classes[ind]
+           re.append(result)
+   
+   return(re)
+
 def predict_input(sentences):
     X_test=[Features.sent_to_features(s,0) for s in sentences]
-    with open("/home/credit/ind_credit/Named_Entity_Recognition/CRF_Classifier.clf", 'rb') as clf:
+    with open("CRF_Classifier.clf", 'rb') as clf:
         crf_classifier = pickle.load(clf)
     prediction=crf_classifier.predict(X_test)
     tag=[]
     word=[]
+ 
     for i in range(len(sentences)):
         for j in range(len(sentences[i])):
           if(prediction[i][j]!='O'):
             word.append(sentences[i][j])
             tag.append(prediction[i][j])
+
     return(word,tag)
 
 def main():
-    f = open('/home/credit/ind_credit/credit_app/static/data/input/bank_statement_232/E-HDFC_1/E-HDFC_1.json',) 
-    data = json.load(f) 
-    for value in data['result'].items():
-        text_list=data['result']['Page_1']['digitized_details']['logical_cells']
-    sentences=[]
-    for i in range(len(text_list)):
-        sentences.append(text_list[i]["token"])
-    sent=[]
-    for i in range(len(sentences)):
-        tokens=[]
-        for j in range(len(sentences[i])):
-            tokens.append(sentences[i][j]['text'])
-        sent.append(tokens)
-    print(sent)
-
-
+   f = open('E-SBI_2.json',) 
+   data = json.load(f) 
+   text_list,sentences,tokens=process_input_file(data)
+   word,tag=predict_input(tokens)
+   result=output(word,tag,sentences,text_list)
+   print(result)
   
-    word,tag=predict_input(sent)
-    result=output(word,tag)
-    print(result)
    
     
 if __name__=="__main__":
